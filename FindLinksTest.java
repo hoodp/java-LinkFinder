26b0b6f9905a8b328cfff6d5d74e6a9f9321e56b
Paul Hood <hoodp@mail.gvsu.edu>
2015-05-21 23:05:24 -0400

    removed system output in tests

--- FindLinksTest.java
+++ FindLinksTest.java
@@ -13,7 +13,6 @@ public class FindLinksTest {
 
   @Test
   public void findLotsOfLinks() throws IOException, SAXException, ParserConfigurationException {
-    System.out.println("Start findLotsOfLinks");
     Document doc = FindLinks.parse("data/lotsOfLinks.html");
     List<String> observed = FindLinks.findLinks(doc);
     String[] expected = {"#bottom", "http://www.google.com", "http://www.microsoft.com", "http://jasmine.github.io", "http://www.gatech.edu", "http://www.gvsu.edu", "http://stackoverflow.com", "http://bubbletimer.com", "https://piazza.com"};
@@ -22,17 +21,14 @@ public class FindLinksTest {
 
   @Test
   public void findLotsOfLinksWithScript() throws IOException, SAXException, ParserConfigurationException {
-    System.out.println("Start findLotsOfLinksWithScript");
     Document doc = FindLinks.parse("data/lotsOfLinksWithScript.html");
     List<String> observed = FindLinks.findLinks(doc);
     String[] expected = {"#bottom", "http://www.google.com", "http://www.microsoft.com", "http://jasmine.github.io", "http://www.gatech.edu", "http://www.gvsu.edu", "http://stackoverflow.com", "http://bubbletimer.com", "https://piazza.com"};
-    System.out.printf("Observed: %d\nExpected: %d\n", observed.size(), expected.length);
     Assert.assertEquals(Arrays.asList(expected), observed);
   }
 
   @Test
   public void noLinks() throws IOException, SAXException, ParserConfigurationException {
-    System.out.println("Start noLinks");
     Document doc = FindLinks.parse("data/noLinks.html");
     List<String> observed = FindLinks.findLinks(doc);
     Assert.assertEquals(new ArrayList<String>(), observed);
