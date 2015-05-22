65497e3df6a163100676acb121a426d62a10c10e
Paul Hood <hoodp@mail.gvsu.edu>
2015-05-22 00:50:50 -0400

    uploaded recursive solution to find classes without using document.getElementBy ...

--- FindClasses.java
+++ FindClasses.java
@@ -1,7 +1,4 @@
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -17,16 +14,47 @@ import java.util.List;
 public class FindClasses {
 
   public static Document parse(String fileName) throws ParserConfigurationException, IOException, SAXException {
+
     // Create a DOM tree (a tree of objects) describing an xhtml document.
     DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
     DocumentBuilder domBuilder = domFactory.newDocumentBuilder();
     return domBuilder.parse(fileName);
   }
 
-  private static List<String> findClasses(Element e, String className, List<String> answer) {
-    //TODO:  Complete this method
+  public static List<String> findClasses(Element e, String className, List<String> answer) {
+
+    // get elements child nodes
+    NodeList nodes = e.getChildNodes();
+
+    // start looping through each child node
+    for (int i = 0; i < nodes.getLength(); i++) {
+
+      // get current node
+      Node node = nodes.item(i);
+
+      // get node attributes
+      NamedNodeMap attr = node.getAttributes();
+
+      // check for null attributes & null class
+      if (attr != null && attr.getNamedItem("class") != null) {
+
+        // search for matching class string
+        if (attr.getNamedItem("class").getTextContent().equals(className)) {
+
+          // add matching id to answer array
+          String id = attr.getNamedItem("id").getTextContent();
+          answer.add(id);
+        }
+      }
+
+      // check if node is an element
+      if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+        // convert node to element and recursive call findClasses
+        Element element = (Element) node;
+        findClasses(element, className, answer);
+      }
+    }
     return answer;
   }
-
-  //
 }
--- FindClassesTest.java
+++ FindClassesTest.java
@@ -4,23 +4,29 @@ import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class FindClassesTest {
 
   @Test
-  public void findSpecial() throws IOException, SAXException, ParserConfigurationException {
+  public void findSpecial() throws IOException, SAXException,
+          ParserConfigurationException {
     String[] expected = {"id2", "id3", "id9", "id15", "id15b", "id16", "id13", "id26"};
-    List<String> observed = FindClasses.findClasses(FindClasses.parse("data/classes.html"), "special");
+    List<String> observed = FindClasses.findClasses(
+            FindClasses.parse("data/classes.html").getDocumentElement(),
+            "special", new ArrayList<String>());
     Assert.assertEquals(Arrays.asList(expected), observed);
   }
 
   @Test
-  public void findOrdinary() throws IOException, SAXException, ParserConfigurationException {
+  public void findOrdinary() throws IOException, SAXException,
+          ParserConfigurationException {
     String[] expected = {"id1", "id4", "id10", "id23", "id37", "id28"};
-    List<String> observed = FindClasses.findClasses(FindClasses.parse("data/classes.html"), "ordinary");
+    List<String> observed = FindClasses.findClasses(
+            FindClasses.parse("data/classes.html").getDocumentElement(),
+            "ordinary", new ArrayList<String>());
     Assert.assertEquals(Arrays.asList(expected), observed);
   }
-
 }
